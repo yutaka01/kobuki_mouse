e8981e56f60236616317249383eddfd03be26124
yutakanaito <44565802+yutakanaito@users.noreply.github.com>
2018-11-15 18:15:13 +0900

    1115

---
+++ scripts/kobuki_controller.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__=="__main__":
+        rospy.init_node('it')
+
+        p=rospy.Publisher('r1/commands/velocity',Twist)
+        twist = Twist()
+        twist.linear.x = -0.5;
+        twist.linear.y = 0.0;
+        twist.linear.z = 0.0;
+
+        twist.angular.x = 0.0;
+        twist.angular.y = 0.0;
+        twist.angular.z = 0.0;
+        for i in range(10):
+                p.publish(twist);
+                rospy.sleep(0.5)
