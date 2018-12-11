bbb0ce749d609679252af44a5aca1bff26bdf91a
yutakanaito <44565802+yutakanaito@users.noreply.github.com>
2018-12-11 20:55:16 +0900

    1211

--- scripts/gotogoal_plus.py
+++ scripts/gotogoal_plus.py
@@ -38,15 +38,15 @@ class turtlebot():
             #Creating our node,publisher and subscriber
             rospy.init_node('turtlebot_controller', anonymous=True)
             self.velocity_publisher = rospy.Publisher('r1/mobile_base/commands/velocity', Twist, queue_size=10)
-            model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+            self.model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
             pts = []
             name = []
             block = _blockListDict["r1"]
             blockName = str(block._name)
-            resp_coordinates = model_coordinates(blockName, block._relative_entity_name)
+            self.resp_coordinates = self.model_coordinates(blockName, block._relative_entity_name)
             #pts = (resp_coordinates.pose.position.x, resp_coordinates.pose.position.y)
             #print(resp_coordinates)
-            rot_q = resp_coordinates.pose.orientation
+            rot_q = self.resp_coordinates.pose.orientation
             global theta
             (roll,pitch,theta) = tf.transformations.euler_from_quaternion([rot_q.x,rot_q.y,rot_q.z,rot_q.w])
             print(theta)
@@ -55,49 +55,59 @@ class turtlebot():
             goal_pose = Point()
             goal_pose.x = 5#input("Set your x goal:")
             goal_pose.y = 5#input("Set your y goal:")
-            distance_tolerance = 0.1
+            distance_tolerance = 0.5
             vel_msg = Twist()
                 
 
         #Porportional Controller
         #linear velocity in the x-axis:
-            u = np.array([resp_coordinates.pose.position.x, resp_coordinates.pose.position.y])
-            v = np.array([goal_pose.x,goal_pose.y])
-            vec = v - u
-            i = vec[0]
-            print(i)
-            n = linalg.norm(vec)
-            c = i / n
-            a = np.arccos(np.clip(c, -1.0, 1.0))
-            if i < 0:
-                a = -a
+            radian = atan2(goal_pose.y - self.resp_coordinates.pose.position.y, goal_pose.x - self.resp_coordinates.pose.position.x)
 
-            vel_msg.linear.x = 0* sqrt(pow((goal_pose.x - resp_coordinates.pose.position.x), 2) + pow((goal_pose.y - resp_coordinates.pose.position.y), 2))
+ 
+            vel_msg.linear.x = 0* sqrt(pow((goal_pose.x - self.resp_coordinates.pose.position.x), 2) + pow((goal_pose.y - self.resp_coordinates.pose.position.y), 2))
             vel_msg.linear.y = 0
             vel_msg.linear.z = 0
 
         #angular velocity in the z-axis:
-            if i > 0:
-                vel_msg.angular.x = 0
-                vel_msg.angular.y = 0
-                vel_msg.angular.z = 1
-            else:
-                vel_msg.angular.x = 0
-                vel_msg.angular.y = 0
-                vel_msg.angular.z = -1
 
-                #Publishing our vel_msg
+            vel_msg.angular.z = 1
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            #Publishing our vel_msg
             self.velocity_publisher.publish(vel_msg)
-            self.rate.sleep()
-            print(u,v,vec,i,a,theta)
+
+            print(theta)
             print(cos(theta))
-            if abs(a - theta) <=0.01:
+            if abs(radian - theta) <=0.005:
                 break
             #Stopping our robot after the movement is over
         vel_msg.linear.x = 0
         vel_msg.angular.z =0
         self.velocity_publisher.publish(vel_msg)
-
+        rospy.sleep(1)
+        #rospy.spin()
+        num = 0
+        #while sqrt(pow((goal_pose.x - self.resp_coordinates.pose.position.x), 2) + pow((goal_pose.y - self.resp_coordinates.pose.position.y), 2)) >= distance_tolerance:
+        while True:
+            num += 1
+            vel_msg.linear.x = 1
+            self.velocity_publisher.publish(vel_msg)
+            #rospy.init_node('turtlebot_controller', anonymous=True)
+            self.velocity_publisher = rospy.Publisher('r1/mobile_base/commands/velocity', Twist, queue_size=10)
+            self.model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+            pts = []
+            name = []
+            block = _blockListDict["r1"]
+            blockName = str(block._name)
+            self.resp_coordinates = self.model_coordinates(blockName, block._relative_entity_name)
+            print(self.resp_coordinates.pose.position.x,self.resp_coordinates.pose.position.y)
+            self.velocity_publisher.publish(vel_msg)
+            self.rate.sleep()
+            if num == 1000:
+                break
+        vel_msg.linear.x = 0
+        vel_msg.angular.z =0
+        self.velocity_publisher.publish(vel_msg)
         rospy.spin()
 
 if __name__ == '__main__':
