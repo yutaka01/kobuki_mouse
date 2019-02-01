a436a86dc81b562c00b02ba0bc2ce3c569d2cc70
yutakanaito <44565802+yutakanaito@users.noreply.github.com>
2019-02-01 19:28:12 +0900

    0201

---
+++ scripts/purasu.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+import random , numpy ,rospy
+import matplotlib.pyplot as plt
+from scipy.spatial import Voronoi, voronoi_plot_2d
+from shapely.geometry import Polygon, Point
+ 
+class Centoro:
+    @classmethod
+    def centroidal(self,vor, pts):
+        sq = Polygon([[0, 0], [100, 0], [100, 50], [0, 50]])
+        maxd = 0.0
+        for i in range(len(pts) - 3):
+            poly = [vor.vertices[v] for v in vor.regions[vor.point_region[i]]]
+            i_cell = sq.intersection(Polygon(poly))
+            p = Point(pts[i])
+            pts[i] = i_cell.centroid.coords[0]
+            d = p.distance(Point(pts[i]))
+        if maxd < d: maxd = d
+        return maxd
+ 
+if __name__ == '__main__':
+    n = 10
+    pts = [[random.uniform(0,100), random.uniform(0,50)] for i in range(n)]
+    for v in range(n):
+        a = v
+        print('X%d=' %(v+1))
+        print(pts[v])#v' = 'pts[v])
+    pts = pts + [[100, 100], [100, -100], [-100, 0]]
+    #pts = pts.replace('"', '')
+    plt.figure(figsize=(6, 6))
+    d_threshold = 0.005
+    num = 0
+    while True:
+        
+        num += 1
+        vor = Voronoi(pts)
+        d = Centoro.centroidal(vor, pts)
+ 
+        plt.cla()
+        fig = voronoi_plot_2d(vor, ax=plt.gca(),show_vertices=False) #ax=plt.gca()
+
+        plt.gca().set_aspect('equal')
+        plt.gca().set_xlim([0, 100])
+        plt.gca().set_ylim([0, 50])
+        if num == 1:
+            plt.savefig(str(num) + '.png', bbox_inches='tight')
+	    plt.show()
+        if d < d_threshold:
+            plt.savefig(str(num) + '.png', bbox_inches='tight')
+            break
+    for v in range(n):
+        print('X%d=' %(v+1))
+        print(pts[v])#v' = 'pts[v])
+    print(num)
+    plt.show()
+
+
